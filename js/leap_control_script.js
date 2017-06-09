a381deb311adc2d8f29743a6d8ad6d8aa6e14444
frank <frankam@inbox.lv>
2017-06-09 12:36:23 +0200

    adds debug & stop on grab

--- js/leap_control_script.js
+++ js/leap_control_script.js
@@ -5,13 +5,21 @@
       for (var i = 0; i < frame.hands.length; i++) {
         var hand = frame.hands[i];
         if (previousFrame && previousFrame.valid) {
-          var translation = hand.translation(previousFrame);
-          y_speed = translation[0] /50;
-          x_speed = translation[1] /250;
+          if(hand.grabStrength < .5){
+            var translation = hand.translation(previousFrame);
+            // console.log(translation);
+            translation[0] = Math.round(translation[0]*100)/100;
+            translation[1] = Math.round(translation[1]*100)/100;
+            document.getElementById('debug').innerHTML = translation[0] + '\n' + translation[1];
+            y_speed = translation[0] /100;
+            x_speed = translation[1] /250;
+          }else {
+            x_speed = 0;
+            y_speed = 0;
+          }
         }
       }
-    }
-    else {
+    }else {
       x_speed = 0;
       y_speed = 0;
     }
